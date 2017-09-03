0d1789c6c5d44b99f0ce6d89ea16a49ac579278e
Roushan <roushan.jha0709@gmail.com>
2017-09-03 21:40:47 +0530

    put DB in prod

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
-  database: 'mongodb://localhost:27017/meanauth',    //dev
+  database: 'mongodb://roushan:roushan@ds123534.mlab.com:23534/meanauth1',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
